086d88526e63e43c21d1c5bd0e9cc76c5e0389e6
lukesherwood <luke.sherwood55@gmail.com>
2020-05-06 21:56:34 +1200

    successfully added bootstrap and added some styling

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -20,3 +20,10 @@ require("channels")
 //= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
+ 
+import "bootstrap";
+import "../stylesheets/application"
+document.addEventListener("turbolinks:load", () => {
+  $('[data-toggle="tooltip"]').tooltip()
+  $('[data-toggle="popover"]').popover()
+})
